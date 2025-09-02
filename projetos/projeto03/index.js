6610269146028358a1790d8de86259ae000caa53
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-09-02 11:40:21 -0300

    fasfas

--- projetos/projeto03/index.js
+++ projetos/projeto03/index.js
@@ -1,17 +1,30 @@
 //Modulos
-
 const express = require('express')
 const app = express()
 const porta = 8085
 const handleBars = require("express-handlebars")
 const mongoose = require("mongoose")
 const path = require("path")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 //Modulos Rotas
 
 const admin = require("./routes/admin.js")
 
 //Config
+
+app.use(session({
+    secret: "andrematos",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.sucessoMSG = req.flash("sucessoMSG")
+    res.locals.errorMSG = req.flash("erroMSG")
+    next()
+})
 
 app.engine('handlebars', handleBars.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -20,6 +33,15 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(express.static(path.join('./public')))
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/blogapp")
+.then(() => {
+    console.log("Conectado com sucesso")
+})
+.catch((err) => {
+    console.log(`Erro ao se conectar ${err}`)
+})
 
 //Rotas
 
--- projetos/projeto03/routes/admin.js
+++ projetos/projeto03/routes/admin.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
+
 router.get('/', (req, res) => {
     res.render("admin/index")
 })
@@ -10,7 +14,47 @@ router.get('/posts', (req, res) => {
 })
 
 router.get("/categorias", (req, res) => {
-    res.send("Categorias")
+    res.render("admin/categorias")
+})
+
+router.get("/categorias/add", (req, res) => {
+    res.render("admin/addcategorias")
+})
+
+router.post("/categorias/nova", (req, res) => {
+    
+    let erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({texto: "Nome inválido."})
+    }
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({texto: "Slug inválido."})
+    }
+
+    if(req.body.nome.length <= 1) {
+        erros.push({texto: "Nome da categoria muito curto."})
+    }
+
+    if (erros.length > 0) {
+        res.render("./admin/addcategorias", {erros: erros})
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save()
+        .then(() => {
+            req.flash("sucessoMSG", "Categoria criada com sucesso")
+            res.redirect("/admin/categorias")
+        })
+        .catch((erro) => {
+            req.flash("erroMSG", "Erro ao salvar a categoria! Tente novamente.")
+            res.redirect("/admin")
+        })
+    }
 })
 
 module.exports = router
